ee568f7631e55f637bf6b6cc1884fd91bfb97d87
Lauri Harjukoski <lauriharjukoski@Lauris-MacBook-Air.local>
2023-01-15 20:48:51 +0200

    Tehtävä 10.23 tehty

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,6 +3,8 @@ import RepositoryItem from './RepositoryItem';
 import ItemSeparator from './ItemSeparator';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
+import {Picker} from '@react-native-picker/picker';
+import { useState } from 'react';
 
 
 
@@ -26,15 +28,27 @@ export const RepositoryListContainer = ({ repositories, handlePress }) => {
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [selectedSorting, setSelectedSorting] = useState("CREATED_AT");
+  const { repositories } = useRepositories(selectedSorting);
+  
   const navigate = useNavigate()
 
   const handlePress = (item) => {
     navigate(`/repositories/${item.id}`)
   }
-
+  
   return (
+  <>
+    <Picker
+      selectedValue={selectedSorting}
+      onValueChange={(itemValue) => setSelectedSorting(itemValue)
+    }>
+      <Picker.Item label="Latest repositories" value="CREATED_AT"/>
+      <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE" />
+      <Picker.Item label="Lowest rated repositories" value="RATING_AVERAGE_ASC" />
+    </Picker>
     <RepositoryListContainer repositories={repositories} handlePress={handlePress}/>
+  </>
   );
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,10 +2,30 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (selectedSorting) => {
+
+  let variables = {"orderBy": "CREATED_AT"}
+
+    switch (selectedSorting) {
+      case "RATING_AVERAGE":
+        variables= {
+          "orderBy": "RATING_AVERAGE"
+        }
+        break
+      case "RATING_AVERAGE_ASC":
+        variables= {
+          "orderBy": "RATING_AVERAGE",
+          "orderDirection": "ASC"
+        }
+        break
+      default:
+        variables = {"orderBy": "CREATED_AT"}
+    }
+
     
 const { data, error, loading} = useQuery(GET_REPOSITORIES, {
   fetchPolicy: 'cache-and-network',
+  variables
 });
 
 const retunrWhileLoading = []
